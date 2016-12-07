2c50bee0746bc1e3637656ecbf4d742f0d91671f
bayu yulfikar <bayuyulfikar01@gmail.com>
2016-12-07 09:33:35 +0800

    delete all

--- penjualan baju/Form1.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Form1
-
-    Private Sub mulaibutton_Click(sender As System.Object, e As System.EventArgs) Handles mulaibutton.Click
-        menuutama.ShowDialog()
-    End Sub
-End Class
